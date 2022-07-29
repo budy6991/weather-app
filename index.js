c2bc9baf107d1c2bec06bcd87e8de514eb5f38d9
budy6991 <alvmenfer@gmail.com>
2022-07-29 13:47:36 +0200

    Object from json working

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello world')
+const makeFetchRequest = async () => {
+    
+    const response = await fetch ('https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=65e349a9474538ac20dbb6d8be331d95',
+    {
+       "mode":"cors"
+    })
+            const responseData = await response.json()
+            
+            console.log(responseData)
+}
+
+makeFetchRequest()
