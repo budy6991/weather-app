bc1ed1e6b02c4ee8e97df30f81f24fc3244670ca
budy6991 <alvmenfer@gmail.com>
2022-07-29 14:10:31 +0200

    Async function working on userInput

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const makeFetchRequest = async () => {
+const makeFetchRequest = async (userInput) => {
     
-    const getCity = await fetch ('http://api.openweathermap.org/geo/1.0/direct?q=madrid&appid=65e349a9474538ac20dbb6d8be331d95',{"mode":"cors"})
-    const data = await getCity.json()
+    const getLanLon = await fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&appid=65e349a9474538ac20dbb6d8be331d95`,{"mode":"cors"})
+    const data = await getLanLon.json()
     const lat = data[0].lat
     const lon = data[0].lon
-    console.log(lat, lon)
     
+    const getCityWeather = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=65e349a9474538ac20dbb6d8be331d95`)
+    const response = await getCityWeather.json()
+
+    console.log(response.main.temp)
 }
 
-makeFetchRequest()
+const userInput = 'London'
+
+makeFetchRequest(userInput)
