7700842ad15cf17f5846d503b28a60f17d78b3f3
budy6991 <alvmenfer@gmail.com>
2022-08-01 17:51:15 +0200

    Search button working with userInput

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const mainContainer = document.getElementById('main-container')
-
 const displayWeather = (...items) => {
-    const weatherContainer = document.createElement('div')
-    weatherContainer.classList.add('weather-container')
-
     items.forEach(item => {
-        console.log(item)
         const div = document.createElement('div')
         div.append(item)
         weatherContainer.append(div)
@@ -22,19 +16,28 @@ const fetchCurrentWeather = async (userInput) => {
     const lon = data[0].lon
     
     const getCityWeather = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=65e349a9474538ac20dbb6d8be331d95&units=metric`)
-
+    
     const response = await getCityWeather.json()
     let currentWeather = `Temperature = ${response.main.temp} °C`
     let feelsLike = `Feels like: ${response.main.feels_like} °C`
     let windSpeed = `Wind: ${response.wind.speed} m/s`
     let humidity = `Humidity: ${response.main.humidity} %`
-
+    
     displayWeather(currentWeather, feelsLike, windSpeed, humidity)
     
 }
 
+const weatherContainer = document.createElement('div')
+weatherContainer.classList.add('weather-container')
+const userInput = document.getElementById('userInput')
+
+document.querySelector('.search').onclick = () => {
+
+    fetchCurrentWeather(userInput.value)
+}
 
 
-const userInput = 'Villaralbo'
+
+const mainContainer = document.getElementById('main-container')
 
 fetchCurrentWeather(userInput)
