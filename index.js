839e2f0008f74e83a8933e91d3fda53241c12bca
budy6991 <alvmenfer@gmail.com>
2022-08-01 19:08:43 +0200

    Fixed bug with removing child nodes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const removeAllChildNodes = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 const displayWeather = (...items) => {
+    
     items.forEach(item => {
         const div = document.createElement('div')
         div.append(item)
         weatherContainer.append(div)
         mainContainer.append(weatherContainer)
-
+        
     })
 }
 
@@ -18,26 +25,37 @@ const fetchCurrentWeather = async (userInput) => {
     const getCityWeather = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=65e349a9474538ac20dbb6d8be331d95&units=metric`)
     
     const response = await getCityWeather.json()
-    let currentWeather = `Temperature = ${response.main.temp} °C`
+    let title = `Weather in ${userInput}`
+    let currentWeather = `Temperature: ${response.main.temp} °C`
     let feelsLike = `Feels like: ${response.main.feels_like} °C`
     let windSpeed = `Wind: ${response.wind.speed} m/s`
     let humidity = `Humidity: ${response.main.humidity} %`
     
-    displayWeather(currentWeather, feelsLike, windSpeed, humidity)
+    displayWeather(title, currentWeather, feelsLike, windSpeed, humidity)
+    
     
 }
 
-const weatherContainer = document.createElement('div')
-weatherContainer.classList.add('weather-container')
-const userInput = document.getElementById('userInput')
+let userInput = document.getElementById('userInput')
+const mainContainer = document.getElementById('main-container')
+const weatherContainer = document.querySelector('.weather-container')
+weatherContainer.style.display = 'none'
+
 
 document.querySelector('.search').onclick = () => {
+    if (weatherContainer.hasChildNodes()) {
+       removeAllChildNodes(weatherContainer)
+       fetchCurrentWeather(userInput.value)
+    }
+    else if (!weatherContainer.hasChildNodes()) {
 
-    fetchCurrentWeather(userInput.value)
+        fetchCurrentWeather(userInput.value)
+        weatherContainer.style.display = 'block'
+        console.log('Hola buenos dias')
+    }
 }
 
 
 
-const mainContainer = document.getElementById('main-container')
 
-fetchCurrentWeather(userInput)
+
