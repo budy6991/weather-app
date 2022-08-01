05420018fa17da30d431eb0bc877b91e92dd7370
budy6991 <alvmenfer@gmail.com>
2022-08-01 14:37:50 +0200

    Added function for display weather info

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
-const makeFetchRequest = async (userInput) => {
+const displayWeather = (...items) => {
+    const weatherContainer = documen.createElement('div')
+    weatherContainer.classList.add('weather-container')
+
+    items.forEach(item => {
+        const div = document.createElement('div')
+        div.append(item)
+        weatherContainer.append(div)
+
+    })
+}
+
+const fetchCurrentWeather = async (userInput) => {
     
     const getLanLon = await fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&appid=65e349a9474538ac20dbb6d8be331d95`,{"mode":"cors"})
     const data = await getLanLon.json()
     const lat = data[0].lat
     const lon = data[0].lon
     
-    const getCityWeather = await fetch (`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=65e349a9474538ac20dbb6d8be331d95&units=metric`)
+    const getCityWeather = await fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=65e349a9474538ac20dbb6d8be331d95&units=metric`)
+
     const response = await getCityWeather.json()
+    let currentWeather = `Temperature = ${response.main.temp} °C`
+    let feelsLike = `Feels like: ${response.main.feels_like} °C`
+    let windSpeed = `Wind: ${response.wind.speed} m/s`
+    let humidity = `Humidity: ${response.main.humidity} %`
 
-    const forecastContainer = document.querySelector('.forecast-container')
+    let info = []
+    info.push(currentWeather, feelsLike, windSpeed, humidity)
 
-    for (let i = 0; i < 8; i++) {
-        const weatherForecast = response.list[i].main.temp
-        forecastContainer.append(weatherForecast)
-        
-    }
+    displayWeather(info)
+    
+    
+
+
 }
 
-//We need to create a function that takes an array and, for each element of the array, creates a li-ul element, that then will be appended to a forecast-container, to show the weather forecast for 8 days, just the temperature.
 
-const userInput = 'Zaragoza'
 
-makeFetchRequest(userInput)
+const userInput = 'Moraleja del vino'
+
+fetchCurrentWeather(userInput)
